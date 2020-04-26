a7583a86544ed24e943e314218a5016a9e56ad9a
Sabaniki <sawada13@mccc.jp>
2020-04-27 01:32:10 +0900

    座標表示スクリプトをDebugに切り出し

---
+++ Assets/Scripts/Debug/PrintObjectPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintObjectPosition : MonoBehaviour {
+    // Start is called before the first frame update
+    private GameObject TrackingObject;
+    public Text PositionText;
+    public string TagText;
+    void Start() {
+        PositionText.text = UpdateRobotPositionText();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (TrackingObject == null) {
+            TrackingObject = GameObject.FindWithTag(TagText);
+            return;
+        }
+        PositionText.text = UpdateRobotPositionText();
+    }
+
+    string UpdateRobotPositionText() {
+        if (TrackingObject == null) {
+            return $"{TagText} Not Found";
+        }
+        Vector3 position;
+        return $"{TagText}Position(X: {(position = TrackingObject.transform.position).x}, Y: {position.y}, Z: {position.z})";
+    }
+}
