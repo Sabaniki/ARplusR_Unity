a736082f07c40e308c6123361a51b44aad50da67
Sabaniki <sawada13@mccc.jp>
2020-04-26 19:21:23 +0900

    Obstacleをデバッグするために全体的な構成を少し変えた

--- Assets/Scripts/Obstacle/PrintObstaclePosition.cs
+++ Assets/Scripts/Obstacle/PrintObstaclePosition.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugObstacle : MonoBehaviour
-{
+public class PrintObstaclePosition : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
